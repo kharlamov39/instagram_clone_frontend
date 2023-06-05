935b03a312dff8e165f2158ed2578033d3516d0d
Kharlamov39 <kharlamov39@yandex.ru>
2023-06-05 21:42:17 +0300

    added process env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'https://instagram-clone-backend-2.onrender.com/'
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 // мидлвар для запросов для вшивания токена
