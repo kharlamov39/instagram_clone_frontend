abf2e7c5bd5cdc7ea6098329577f9cbc633e07f3
Kharlamov39 <kharlamov39@yandex.ru>
2023-06-27 12:03:01 +0300

    added headers

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:1111/'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:1111/',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Credentials': 'true'
+      }
 })
 
 // мидлвар для запросов для вшивания токена
