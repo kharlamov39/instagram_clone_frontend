c37fd143b6d4361b33d15e5b2635a8270d539021
Kharlamov39 <kharlamov39@yandex.ru>
2023-06-29 15:20:32 +0300

    added env local

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:1111/',
+    baseURL: process.env.REACT_APP_API_URL,
 })
 
 // мидлвар для запросов для вшивания токена
