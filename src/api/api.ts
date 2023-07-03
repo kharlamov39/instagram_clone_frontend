85eb459d0490e399a8650042977f689a371c52c2
Kharlamov39 <kharlamov39@yandex.ru>
2023-07-03 14:10:04 +0300

    fix css

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const instance = axios.create({
     // baseURL: 'https://instagram-clone-backend-2.onrender.com',
-    // baseURL: `http://localhost:1111`
-    baseURL: `${process.env.REACT_APP_API_URL}`
+    baseURL: `http://localhost:1111`
+    // baseURL: `${process.env.REACT_APP_API_URL}`
 })
 
 // мидлвар для запросов для вшивания токена
